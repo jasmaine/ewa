9acb2cad30f7e713e5b424e466a52b43d3300e9a
Mike Pasarella <pasarella@mikes-macbook-pro.local>
2021-03-17 16:58:45 +0000

    Add orderby method + added comment blocks

--- app/lib/classes/mysql.class.php
+++ app/lib/classes/mysql.class.php
@@ -1,18 +1,30 @@
 <?php
 
-
+/**
+ * Mysql class
+ */
 class Mysql
 {
-	
     protected static $where;
     protected static $limit;
+    protected static $orderby;
 
+    /**
+     * Start database connection and store in object
+     * @return void
+     */
     public function __construct()
     {
         $this->db = \Database::initConnection();
     }
     
-
+    /**
+     * Protected static for method 'where'
+     *
+     * @param string|array $info
+     * @param string $type
+     * @return string self::$where
+     */
     protected static function _where($info, $type = 'AND')
     {
         foreach ($info as $row => $value) {
@@ -30,7 +42,13 @@ class Mysql
         self::$where = $where;
     }
     
-    
+    /**
+     * Protected static for method 'limit'
+     *
+     * @param int $from
+     * @param int $to
+     * @return string self::$limit
+     */
     protected static function _limit($from, $to)
     {
         if ($to == null) {
@@ -42,7 +60,48 @@ class Mysql
         self::$limit = $limit;
     }
     
-
+    /**
+     * Protected static for method 'orderby'
+     *
+     * @param string $field
+     * @param string $order
+     * @return string self::$orderby
+     */
+    protected static function _orderby($field, $order)
+    {
+        if ($field == "") {
+            self::$orderby = null;
+        } else {
+            self::$orderby = sprintf(" ORDER BY %s %s", $field, $order);
+        }
+    }
+    
+    /**
+     * Protected static to build extra select option for the get query
+     *
+     * @return string $extra
+     */
+    protected static function _extra()
+    {
+        $extra = '';
+        
+        if (self::$orderby != null) {
+            $extra .= self::$orderby;
+        }
+        if (self::$limit != null) {
+            $extra .= self::$limit;
+        }
+        
+        return $extra;
+    }
+    
+    /**
+     * Method database where
+     *
+     * @param string|array $field
+     * @param string $equal
+     * @return $this
+     */
     public function where($field, $equal = null)
     {
         if (is_array($field)) {
@@ -54,21 +113,50 @@ class Mysql
         return $this;
     }
     
-    
+    /**
+     * Method database limit
+     *
+     * @param int $from
+     * @param int $to
+     * @return $this
+     */
     public function limit($from, $to = null)
     {
         self::_limit($from, $to);
         
         return $this;
     }
+    
+    /**
+     * Method database orderby
+     *
+     * @param string $field
+     * @param string $order
+     * @return $this
+     */
+    public function orderby($field, $order = 'ASC')
+    {
+        if ($order != 'DESC' || $order != 'ASC') {
+            $order = 'ASC';
+        }
+        self::_orderby($field, $order);
+        
+        return $this;
+    }
 
-
+    /**
+     * Method database SELECT
+     *
+     * @param string $table
+     * @param string $select
+     * @return boolean false|string $data
+     */
     public function get($table, $select = '*')
     {
         $where = '';
         $type = '';
         $params = false;
-        $limit = '';
+        $extra = self::_extra();
         
         if (self::$where != null) {
             $where = self::$where['clause'];
@@ -76,11 +164,7 @@ class Mysql
             $type = self::$where['type'];
         }
         
-        if (self::$limit != null) {
-            $limit = self::$limit;
-        }
-        
-        $sql = "SELECT $select FROM $table".$where.$limit;
+        $sql = "SELECT $select FROM $table".$where.$extra;
         
         $stmt = $this->db->prepare($sql);
 
