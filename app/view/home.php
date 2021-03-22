6868527a7b6b65e148c02934e4fa2813b5163b3e
Mike Pasarella <pasarella@mikes-macbook-pro.local>
2021-03-22 17:59:54 +0000

    Adding a homepage + fixed CSS fonts paths errors.

--- app/view/home.php
+++ app/view/home.php
@@ -1,6 +1,16 @@
-<?php
-    
-echo trans('button.login');
-    
-?>
-<h1>Mike</h1>
+<div class="container container-center">
+    <div class="grid">
+        <div class="width-1-2">
+            <h1>Hello EWA!</h1>
+            <p>
+                Thank you for choosing EWA as your web application core.
+                When you see this message, it means the basic settings are setup correctly.
+            </p>
+            <strong>Keep update</strong>
+            <p>
+                Follow the newest releases on <i class="icon-github"></i> <a href="https://github.com/jasmaine/ewa" class="underline">GitHub</a>. From this moment the installation needs to be done fully manual. Need help? Feel free to send a message.
+                <br /><br />
+                <a href="https://github.com/jasmaine/ewa" class="text-blue-700">https://github.com/jasmaine/ewa</a>
+            </p>
+        </div>
+    </div>
