d667395b71e98c1e59b1bef962e8ab9d0ebfe8da
Mike Pasarella <pasarella@mikes-macbook-pro.local>
2021-03-22 18:25:40 +0000

    Settings file update

--- app/lib/config/basic.conf.php
+++ app/lib/config/basic.conf.php
@@ -1,27 +1,28 @@
 <?php
 
 /**
- * DB Login settings
- */
-define("DBHOST", "localhost");
-define("DBNAME", "game");
-define("DBUSER", "root");
-define("DBPASS", "enter");
-
-/**
  * App settings
  */
 define("PROCESSED", true);
 define("APPLOC", "app");
+define("TEMPLATE_PLAIN", "404,post,get");
+
+/**
+ * Database settings
+ */
+define("DBHOST", "localhost");
+define("DBNAME", "games");
+define("DBUSER", "root");
+define("DBPASS", "enter");
 
 /*
- * Basic website settings
+ * Website settings
  */
 define("START_PAGE", "home");
-define("URI", "http://ewa.project.com");
-define("WEBSITE_BASIC_LANG", "nl");
+define("URI", "https://www.yourdomain.com");
+define("WEBSITE_BASIC_LANG", "en");
 define("WEBSITE_TITLE", "Website Title");
-define("WEBSITE_AUTHOR", "Mike Pasarella");
+define("WEBSITE_AUTHOR", "Your Name");
 
 /*
  * SMTP Mail Settings
@@ -32,12 +33,3 @@ define("MAIL_PASS", "");
 define("MAIL_PORT", 587);
 define("MAIL_FROM_ADDRESS", "");
 define("MAIL_FROM_NAME", "");
-
-/**
- * Template Settings
- *
- * standard every page will get a header and footer template
- * If pages does not require them, add them to the list below
- * use a ',' to separate the pages
- */
-define("TEMPLATE_PLAIN", "404,post,get");
