71b5764826596836fa5a1db8b6c19977f3c864e3
Mike Pasarella <pasarella@mikes-macbook-pro.local>
2021-03-27 12:54:18 +0000

    Add a possibilty to pass an array of placeholder text.

--- app/lib/classes/lang.class.php
+++ app/lib/classes/lang.class.php
@@ -6,7 +6,6 @@
 
 class Lang
 {
-
     /**
      * Return the language file path
      *
@@ -15,14 +14,14 @@ class Lang
     public static function getFileByLang()
     {
         $userLang = Lang::getUserLang();
-        
+
         if (file_exists(BASEPATH.APPLOC.'/lib/config/lang/'.$userLang.'.lang.php')) {
             return include(BASEPATH.APPLOC.'/lib/config/lang/'.$userLang.'.lang.php');
         } else {
             return include(BASEPATH.APPLOC.'/lib/config/lang/'.WEBSITE_BASIC_LANG.'.lang.php');
         }
     }
-        
+
     /**
      * Return the language key=>value
      *
@@ -36,25 +35,36 @@ class Lang
             return substr($_SERVER['HTTP_ACCEPT_LANGUAGE'], 0, 2);
         }
     }
-    
+
     /**
      * Get the translation value based on a given key
      *
      * @param string $key
      * @return string
      */
-    public static function get($key)
+    public static function get($key, $value = null)
     {
+        // Explode the keys
         $keys = explode(".", $key);
-        
+
+        // Get the complete translation file as array
         $lang = Lang::getFileByLang();
-        
+
+        // Search the key value
         for ($i = 0;$i < count($keys) ;$i++) {
             if (array_key_exists($keys[$i], $lang)) {
                 $lang = $lang[$keys[$i]];
             }
         }
-        
+
+        // Replace dynamic values
+        if (is_array($value)) {
+            foreach ($value as $k=>$v) {
+                $lang = str_replace('{'.$k.'}', $v, $lang);
+            }
+        }
+
+        // Return the translation
         if (!is_array($lang)) {
             return $lang;
         }
