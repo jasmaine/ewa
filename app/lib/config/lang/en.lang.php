750130f024885ecfd645058518a66e5ee7d13a81
Mike Pasarella <pasarella@mikes-macbook-pro.local>
2021-03-27 12:53:57 +0000

    Add new language

--- app/lib/config/lang/en.lang.php
+++ app/lib/config/lang/en.lang.php
@@ -4,10 +4,15 @@
  * English Translations
  */
 return [
+    'error_message_head' => 'Error message',
     'page_not_found' => 'We could not find the requested page.',
     'back_to_home' => 'Back to the homepage',
     'button' => [
         'login' => 'Login',
         'register' => 'Register',
     ],
+    'password' => [
+        'too_short' => 'Password is too short and needs a minimum of {length} charachters.',
+        'need_all' => 'Password need to contain at least a lower and upper case letter, a number and a special charachter.',
+    ]
 ];
--- app/lib/config/lang/nl.lang.php
+++ app/lib/config/lang/nl.lang.php
@@ -4,10 +4,15 @@
  * Dutch Translations
  */
 return [
+    'error_message_head' => 'Foutmelding',
     'page_not_found' => 'We konden de opgevraagde pagina niet vinden.',
     'back_to_home' => 'Terug naar de homepage',
     'button' => [
         'login' => 'Inloggen',
         'register' => 'Registreren',
+    ],
+    'password' => [
+        'too_short' => 'Password dient minimaal {length} karakters te bevatten.',
+        'need_all' => 'Wachtwoord moet bestaan uit een hoofdletter, een kleine letter, een cijfer en een speciaal teken.',
     ],
 ];
