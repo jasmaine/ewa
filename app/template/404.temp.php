9f866a16105e54120bdfef7243dff270a94b1f03
Mike Pasarella <pasarella@mikes-macbook-pro.local>
2021-04-10 12:29:05 +0000

    Update template

--- app/template/404.temp.php
+++ app/template/404.temp.php
@@ -7,26 +7,12 @@
    <meta name="author" http-equiv="author" content="<?php echo WEBSITE_AUTHOR; ?>" />
    <meta name="robots" content="noindex" />
    <title>404 | <?php echo TITLE; ?></title>
-   <script type="text/javascript" src="<?php echo URI; ?>/assets/js/jquery.2.min.js"></script>
-   <script type="text/javascript" src="<?php echo URI; ?>/assets/js/app.js"></script>
-   <link rel="stylesheet" type="text/css" media="all" href="<?php echo URI; ?>/assets/css/jasmine.1.min.css" />
-   <link rel="stylesheet" type="text/css" media="all" href="<?php echo URI; ?>/assets/css/fonts.css" />
-   <link rel="stylesheet" type="text/css" media="all" href="<?php echo URI; ?>/assets/css/app.custom.css" />
 </head>
 
 <body>
-   <div class="container">
-      <div class="grid">
-         <div class="width-1-1">
-            <div class="pad-40">
-               <div class="extreme light">404</div>
-               <div class="medium light"><?php echo trans('page_not_found'); ?><br />
-                  <a href="home" class="text-cyan small"><?php echo trans('back_to_home'); ?></a>
-               </div>
-            </div>
-         </div>
-      </div>
-   </div>
+   <h1>404</h1>
+   <p><?php echo trans('page_not_found'); ?></p>
+   <p><a href="home" class="text-cyan small"><?php echo trans('back_to_home'); ?></a></p>
 </body>
 
 </html>
--- app/template/header.temp.php
+++ app/template/header.temp.php
@@ -9,11 +9,6 @@
    <meta name="author" http-equiv="author" content="<?php echo WEBSITE_AUTHOR; ?>" />
    <meta name="robots" content="index,follow" />
    <title><?php echo TITLE; ?></title>
-   <script type="text/javascript" src="<?php echo URI; ?>/assets/js/jquery.2.min.js"></script>
-   <script type="text/javascript" src="<?php echo URI; ?>/assets/js/app.js"></script>
-   <link rel="stylesheet" type="text/css" media="all" href="<?php echo URI; ?>/assets/css/jasmine.1.min.css" />
-   <link rel="stylesheet" type="text/css" media="all" href="<?php echo URI; ?>/assets/css/app.custom.css" />
-   <link rel="stylesheet" type="text/css" media="all" href="<?php echo URI; ?>/assets/css/fonts.css" />
 </head>
 
 <body>
